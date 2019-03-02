e4559e6a551d5327b8285befe1c517a1f293c45c
cga1910 <cga1910@users.noreply.github.com>
2019-03-02 02:09:10 +0100

    Update Main.java

--- skal_gustav/Main.java
+++ skal_gustav/Main.java
@@ -1,8 +1,8 @@
 public class Main {
   
-	public static void main(String[] args) {
-      mainMenu();
-	}
+  public static void main(String[] args) {
+    mainMenu();
+  }
 
   private static void mainMenu() {
     boolean looping = true;
