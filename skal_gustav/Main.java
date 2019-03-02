696d7e986f002a0c75462b012ca6261e5c95e1c9
gso1910 <gustav.bj@gmail.com>
2019-03-02 03:04:08 +0100

    Menu navigation test

--- skal_gustav/Main.java
+++ skal_gustav/Main.java
@@ -1,21 +1,21 @@
 public class Main {
   
   public static void main(String[] args) {
-    mainMenu();
+    try {
+      mainMenu();
+    } catch (Exception e) {
+      System.out.println(e);
+    }
   }
 
   private static void mainMenu() {
-    boolean looping = true;
-    while (looping) {
+    while (true) {
       System.out.println('\n' + "1. Kund" + '\n' + "2. Admin");
       int userInput = Input.userInputInt();
       if (userInput == 1) {
         Customer.menu();
       } else if (userInput == 2) {
         Admin.menu();
-      } else if (userInput == 0) {
-        System.out.println("Avslutar...");
-        looping = false;
       }
     }
   }
