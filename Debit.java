d581e324cfe31fa0c47c9bb1642a1bd107cf6470
gso1910 <gustav.bj@gmail.com>
2019-03-09 00:29:50 +0100

    Cash payment code, and other implementations that was necessary for cash payment

---
+++ Debit.java
@@ -0,0 +1,18 @@
+public class Debit {
+
+  // Eftersom vi aldrig behöver flera debit-objekt samtidigt (vi ska alltid
+  // ha enbart ett pris, etc), låter vi variablerna vara klassvariabler (static)
+  // gissar jag.
+  static double timeUnit = 900;
+  static double taxa = 10;
+  static double ADT = -1;
+  static double debit = -1;
+
+  public static double getDebit(double parkedTime) {
+    ADT = parkedTime / timeUnit;
+    ADT = Math.ceil(ADT); // Avrunda uppåt
+    debit = ADT * taxa;
+    return debit;
+  }
+
+}
--- ParkedVehicle.java
+++ ParkedVehicle.java
@@ -15,9 +15,19 @@ public class ParkedVehicle {
 
   // Fördefinierade start-tider
   public static ArrayList<Integer> startTimeList = new ArrayList<Integer>
+          (Arrays.asList(Garage.systemTime, Garage.systemTime,
+                  Garage.systemTime, Garage.systemTime, Garage.systemTime,
+                  Garage.systemTime, Garage.systemTime, Garage.systemTime,
+                  Garage.systemTime, Garage.systemTime, Garage.systemTime,
+                  Garage.systemTime, Garage.systemTime, Garage.systemTime,
+                  Garage.systemTime, Garage.systemTime, Garage.systemTime,
+                  Garage.systemTime, Garage.systemTime, Garage.systemTime));
+
+  /*
+  public static ArrayList<Integer> startTimeList = new ArrayList<Integer>
           (Arrays.asList(900, 1200, 1900, 2400, 3200, 4000, 6000, 7000, 8000,
                   9000, 10000, 11000, 12000, 13000, 14000, 15000, 16000, 17000,
-                  18000, 19000));
+                  18000, 19000)); */
 
   // Konstruktor för parkerade fordon
   public ParkedVehicle(String regNr, int startTime) {
